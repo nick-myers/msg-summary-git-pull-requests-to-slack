145db8f6e7efc620d570a1f5500c3822a6a30216
Nick Myers <myers.nick@gmail.com>
2019-10-24 11:23:23 +0100

    Change sack message to include @here

--- src/g2s.ts
+++ src/g2s.ts
@@ -81,7 +81,7 @@ async function sleep(ms: number){
 async function slack(message: string) {
     await web.chat.postMessage({
         channel: channel,
-        text: message
+        text: '@here' + message
     });
 }
 async function query() {
